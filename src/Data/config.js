8cd6118d407a3eadb32b821d24f2a7524f6f5f73
RiteshRana25 <160099737+RiteshRana25@users.noreply.github.com>
2024-12-20 19:35:34 +0530

    Update config.js

--- src/Data/config.js
+++ src/Data/config.js
@@ -1,4 +1,5 @@
-const apikey='0a1221d85a354e9baf274e17fac031ec'
+const apikey='b71b50d731a54d88957f9cf292763d2b'
+// '0a1221d85a354e9baf274e17fac031ec'
 // '2483156344234b0ebca5b49caf6a5a69'
 // 'b71b50d731a54d88957f9cf292763d2b'
 export default apikey;
