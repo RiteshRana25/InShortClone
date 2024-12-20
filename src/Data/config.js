b577500b0620b3909ca1f2c56b26d5608ddbeea7
unknown <riteshraba251104@gmail.com>
2024-12-20 19:52:47 +0530

    Update config.js with new changes

--- src/Data/config.js
+++ src/Data/config.js
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 const apikey='2483156344234b0ebca5b49caf6a5a69';
-=======
-const apikey='b71b50d731a54d88957f9cf292763d2b'
-// '0a1221d85a354e9baf274e17fac031ec'
->>>>>>> 8cd6118d407a3eadb32b821d24f2a7524f6f5f73
-// '2483156344234b0ebca5b49caf6a5a69'
 // 'b71b50d731a54d88957f9cf292763d2b'
 // '0a1221d85a354e9baf274e17fac031ec'
 export default apikey;
