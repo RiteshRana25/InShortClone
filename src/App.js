78212dd6c17eae2783ecb7e39488c7903a7a6ec6
unknown <riteshraba251104@gmail.com>
2024-12-22 13:19:50 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,61 +1,48 @@
 import './App.css';
-import { useState, useEffect } from 'react';
+import { useState ,useEffect} from 'react';
 import NavBarInshort from './component/NavBarInshort/NavBarInshort';
 import axios from "axios";
 import NewsContent from './component/newsContent/newsContent';
+import NewsCard from './component/newsCard/newsCard';
 import Footer from './component/footer/footer';
 import apikey from './Data/config';
 
 function App() {
-  const [category, setCategory] = useState("general");  
-  const [newsArray, setNewsArray] = useState([]);
-  const [newsResult, setNewsResult] = useState();
-  const [lodeMore, setLodeMore] = useState(10);
+  const [category, setCategory] = useState(["general"])
+  const [newsArray, setNewsArray] = useState([])
+  const [newsResult, setNewsResult] = useState()
+  const [lodeMore, setLodeMore] = useState(10)
 
-  const newsApi = async () => {
+  const newsApi=async ()=>{
     try {
-      const proxyUrl = "https://cors-anywhere.herokuapp.com/";
-      const response = await axios.get(`${proxyUrl}https://newsapi.org/v2/top-headlines`, {
-        params: {
-          country: 'us',
-          category: category,
-          apiKey: apikey,
-          pageSize: lodeMore,
-        },
-      });
-      if (response.data && response.data.articles) {
-        const filteredArticles = response.data.articles.filter(
+      const proxyUrl="https://cors-anywhere.herokuapp.com"
+      const news = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${apikey}&pageSize=${lodeMore}`);
+      if (news.data && news.data.articles) {
+        const filteredArticles = news.data.articles.filter(
           (article) => article.title !== '[Removed]'
         );
         setNewsArray(filteredArticles);
       } else {
         setNewsArray([]);
       }
-      setNewsResult(response.data.totalResults);
+      setNewsResult(news.data.totalResults)
     } catch (error) {
       console.log(error);
     }
-  };
-
+  }
   useEffect(() => {
-    newsApi();
-    // eslint-disable-next-line
-  }, [newsArray,newsResult, lodeMore]); 
-
+  newsApi();  // eslint-disable-next-line
+  }, [newsArray,newsResult,lodeMore])
+  
   return (
     <>
-      <nav>
-        <NavBarInshort setCategory={setCategory} />
-      </nav>
-      <NewsContent
-        lodeMore={lodeMore}
-        setLodeMore={setLodeMore}
-        newsArray={newsArray}
-        newsResult={newsResult}
-      />
-      <footer>
-        <Footer />
-      </footer>
+    <nav>
+    <NavBarInshort setCategory={setCategory}></NavBarInshort>
+    </nav>
+    <NewsContent lodeMore={lodeMore} setLodeMore={setLodeMore} newsArray={newsArray} newsResult={newsResult}></NewsContent>
+    <footer>
+      <Footer></Footer>
+    </footer>
     </>
   );
 }
